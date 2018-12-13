f4ae4da62b4f483a6b50ec5d41dc7c53a0d46874
Lucas Song <yuanzheng@cs.byu.edu>
2018-12-13 22:08:39 +0800

    Created DbOutputWritable class

---
+++ TextPrediction/src/main/java/DBOutputWritable.java
@@ -0,0 +1,11 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.hadoop.mapreduce.lib.db.DBWritable;
+
+public class DBOutputWritable implements DBWritable {
+
+
+
+}
