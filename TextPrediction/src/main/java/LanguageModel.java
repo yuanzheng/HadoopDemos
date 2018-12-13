0a3ed032cd064ed9ac0f6f6ee3b99e4c67afe963
Lucas Song <yuanzheng@cs.byu.edu>
2018-12-13 22:07:00 +0800

    Setup MapReduce template for Language Model

---
+++ TextPrediction/src/main/java/LanguageModel.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+public class LanguageModel {
+
+
+    public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+
+    }
+
+
+    public static class Reduce extends Reducer<Text, Text, DBOutputWritable, NullWritable> {
+
+    }
+
+}
