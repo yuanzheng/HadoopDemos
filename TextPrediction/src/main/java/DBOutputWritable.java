2fba522946778407305809ec011fb0dfe061c999
Lucas Song <yuanzheng@cs.byu.edu>
2018-12-16 01:21:38 +0800

    Implemented class for DBwritable

--- TextPrediction/src/main/java/DBOutputWritable.java
+++ TextPrediction/src/main/java/DBOutputWritable.java
@@ -1,11 +1,34 @@
+import org.apache.hadoop.mapreduce.lib.db.DBWritable;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.apache.hadoop.mapreduce.lib.db.DBWritable;
-
 public class DBOutputWritable implements DBWritable {
 
+    private String inputPhrase;
+    private String followingWord;
+    private int count;
 
+    public DBOutputWritable(String startingPhrase, String followingWord, int count) {
+
+        this.inputPhrase = startingPhrase;
+        this.followingWord = followingWord;
+        this.count = count;
+    }
+
+    public void readFields(ResultSet arg0) throws SQLException {
+
+        this.inputPhrase = arg0.getString(1);
+        this.followingWord = arg0.getString(2);
+        this.count = arg0.getInt(3);
+    }
+
+    public void write(PreparedStatement arg0) throws SQLException {
+
+        arg0.setString(1, this.inputPhrase);
+        arg0.setString(2, this.followingWord);
+        arg0.setInt(3, this.count);
+    }
 
 }
