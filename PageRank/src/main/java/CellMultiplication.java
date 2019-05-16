cc965cc22f72489ba90fd28dcb9e8c74a1f51671
Lucas Song <yuanzheng@cs.byu.edu>
2019-05-16 17:29:15 +0800

    built up the project outline.

---
+++ PageRank/src/main/java/CellMultiplication.java
@@ -0,0 +1,52 @@
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+
+/** Transition matrix cell * PR matrix cell
+ */
+public class CellMultiplication {
+
+    /** Generate transition matrix cell
+     *
+     */
+    public static class TransitionMapper extends Mapper<Object, Text, Text, Text> {
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+            //input format: fromPage\t toPage1,toPage2,toPage3
+            //target: build transition matrix unit -> fromPage\t toPage=probability
+        }
+    }
+
+    /** Generate PR matrix cell
+     *
+     */
+    public static class PRMapper extends Mapper<Object, Text, Text, Text> {
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+            //input format: Page\t PageRank
+            //target: write to reducer
+        }
+    }
+
+    /** Transition matrix cell * PR matrix cell
+     *
+     */
+    public static class MultiplicationReducer extends Reducer<Text, Text, Text, Text> {
+
+
+        @Override
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+
+            //input key = fromPage value=<toPage=probability..., pageRank>
+            //target: get the unit multiplication
+        }
+    }
+}
---
+++ PageRank/src/main/java/CellSum.java
@@ -0,0 +1,36 @@
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+
+/** Sum up cell for each webpage
+ *
+ */
+public class CellSum {
+
+    public static class PassMapper extends Mapper<Object, Text, Text, DoubleWritable> {
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+            //input format: toPage\t unitMultiplication
+            //target: pass to reducer
+        }
+    }
+
+    public static class SumReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+
+        @Override
+        public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
+                throws IOException, InterruptedException {
+
+            //input key = toPage value = <unitMultiplication>
+            //target: sum!
+        }
+    }
+
+}
