51dd7befb18a3b971919bc3ef4c331577ee39461
Lucas Song <yuanzheng@cs.byu.edu>
2019-06-18 23:41:41 +0800

    Building co-Occurrence Matrix generator MapReduce class

---
+++ RecommenderSystem/src/main/java/CoOccurrenceMatrixGenerator.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class CoOccurrenceMatrixGenerator {
+
+    public static class MatrixGeneratorMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+
+        }
+    }
+
+    public static class MatrixGeneratorReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf);
+        job.setMapperClass(MatrixGeneratorMapper.class);
+        job.setReducerClass(MatrixGeneratorReducer.class);
+
+        job.setJarByClass(CoOccurrenceMatrixGenerator.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        TextInputFormat.setInputPaths(job, new Path(args[0]));
+        TextOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+
+    }
+}
