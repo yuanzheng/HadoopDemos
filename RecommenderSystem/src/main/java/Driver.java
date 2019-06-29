e1f8970d2fb20ae2985e8316f8d554a74bc3c404
Lucas Song <yuanzheng@cs.byu.edu>
2019-06-29 15:59:49 +0800

    Updated Drive class for CoOccurrenceMatrix Generator

--- RecommenderSystem/src/main/java/Driver.java
+++ RecommenderSystem/src/main/java/Driver.java
@@ -4,24 +4,23 @@ public class Driver {
     public static void main(String[] args) throws Exception {
 
         DataDividerByUser dataDividerByUser = new DataDividerByUser();
-        /*
         CoOccurrenceMatrixGenerator coOccurrenceMatrixGenerator = new CoOccurrenceMatrixGenerator();
-        Normalization normalization = new Normalization();
+        //Normalization normalization = new Normalization();
 
-         */
         /* TODO Multiplication and Sum */
 
 
         String rawInput = args[0];
         String userMovieListOutputDir = args[1];
-        //String coOccurrenceMatrixOutputDir = args[2];
+        String coOccurrenceMatrixOutputDir = args[2];
         //String normalizationDir = args[3];
 
         String[] path1 = {rawInput, userMovieListOutputDir};
-        //String[] path2 = {userMovieListOutputDir, coOccurrenceMatrixOutputDir};
+        String[] path2 = {userMovieListOutputDir, coOccurrenceMatrixOutputDir};
         //String[] path3 = {coOccurrenceMatrixOutputDir, normalizationDir};
 
         DataDividerByUser.main(path1);
+        CoOccurrenceMatrixGenerator.main(path2);
 
     }
 
