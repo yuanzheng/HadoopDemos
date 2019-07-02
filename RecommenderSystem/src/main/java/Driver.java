c50a9a0b1a04e5e74c5e4b4dc6e03e4dcb361730
Lucas Song <yuanzheng@cs.byu.edu>
2019-07-02 12:14:05 +0800

    Implemented the Multiplication MapReduce

--- RecommenderSystem/src/main/java/Driver.java
+++ RecommenderSystem/src/main/java/Driver.java
@@ -7,21 +7,25 @@ public class Driver {
         CoOccurrenceMatrixGenerator coOccurrenceMatrixGenerator = new CoOccurrenceMatrixGenerator();
         //Normalization normalization = new Normalization();
 
-        /* TODO Multiplication and Sum */
-
-
         String rawInput = args[0];
         String userMovieListOutputDir = args[1];
         String coOccurrenceMatrixOutputDir = args[2];
         String normalizationDir = args[3];
+        String multiplicationDir = args[4];
+        String sumDir = args[5];
 
         String[] path1 = {rawInput, userMovieListOutputDir};
         String[] path2 = {userMovieListOutputDir, coOccurrenceMatrixOutputDir};
         String[] path3 = {coOccurrenceMatrixOutputDir, normalizationDir};
+        String[] path4 = {normalizationDir, rawInput, multiplicationDir};
+        String[] path5 = {multiplicationDir, sumDir};
 
         DataDividerByUser.main(path1);
         CoOccurrenceMatrixGenerator.main(path2);
         Normalization.main(path3);
+        MatrixMultiplication.main(path4);
+
+        /* TODO Multiplication and Sum */
 
     }
 
--- RecommenderSystem/src/main/java/MatrixMultiplication.java
+++ RecommenderSystem/src/main/java/MatrixMultiplication.java
@@ -1,4 +1,6 @@
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -9,40 +11,39 @@ import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-import org.apache.hadoop.fs.Path;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
-public class MatrixMultiplication {
-
-    public static class CooccurrenceMapper extends Mapper<LongWritable, Text, Text, Text> {
-
-        @Override
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-
-        }
-    }
-
-    public static class RatingMapper extends Mapper<LongWritable, Text, Text, Text> {
-
-        @Override
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-
-        }
-    }
-
-    public static class MultiplicationReducer extends Reducer<Text, Text, Text, DoubleWritable> {
-
-        @Override
-        public void reduce(Text key, Iterable<Text> values, Context context)
-                throws IOException, InterruptedException {
-
-        }
-    }
-
+public class MatrixMultiplication extends Configured implements Tool {
 
     public static void main(String[] args) throws Exception {
+
+        int exitCode = ToolRunner.run(new MatrixMultiplication(), args);
+
+        if (exitCode != 0) {
+            System.err.printf("Failed, MatrixMultiplication causes the termination\n");
+            System.exit(exitCode);
+        }
+    }
+
+    @Override
+    public int run(String[] args) throws ClassNotFoundException, IOException, InterruptedException {
+
+        if (args.length < 2) {
+            System.err.printf("Usage: hadoop jar RecommenderSystem-jar-with-dependencies.jar <input files> " +
+                            "<UserMovieListOutput Directory> <Co-OccurrenceMatrixOutput Direcory> " +
+                            "<Normalization Directory> <Multiplication Directory> <> [generic options]\n" +
+                            "Here, the <input file> and <MatrixMultiplication Output Directory> are missing!\n",
+                    getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
+        }
+
         Configuration conf = new Configuration();
 
         Job job = Job.getInstance(conf);
@@ -50,7 +51,7 @@ public class MatrixMultiplication {
         job.setJarByClass(MatrixMultiplication.class);
 
         ChainMapper.addMapper(job, CooccurrenceMapper.class, LongWritable.class, Text.class, Text.class, Text.class, conf);
-        ChainMapper.addMapper(job, RatingMapper.class, Text.class, Text.class, Text.class, Text.class, conf);
+        ChainMapper.addMapper(job, RatingMapper.class, LongWritable.class, Text.class, Text.class, Text.class, conf);
 
         job.setMapperClass(CooccurrenceMapper.class);
         job.setMapperClass(RatingMapper.class);
@@ -67,6 +68,104 @@ public class MatrixMultiplication {
 
         TextOutputFormat.setOutputPath(job, new Path(args[2]));
 
-        job.waitForCompletion(true);
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    /** First Mapper loads Co-Occurrence matrix
+     *
+     */
+    public static class CooccurrenceMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+            //input: movieB \t movieA=relation
+            String[] input = value.toString().trim().split("\t");
+            if (input.length < 2) {
+                return;
+            }
+
+            context.write(new Text(input[0]), new Text(input[1]));
+        }
+    }
+
+    /** Second Mapper loads the raw data file in which it includes user_id, movie_id and Rating
+     *
+     */
+    public static class RatingMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+            //input[3]: userID,movieID,rating
+            String[] input = value.toString().trim().split(",");
+            if (input.length < 3) {
+                return;
+            }
+
+            String movieId = input[1];
+            String movieRating = input[0] + ":" + input[2];
+
+            context.write(new Text(movieId), new Text(movieRating));
+        }
+    }
+
+    public static class MultiplicationReducer extends Reducer<Text, Text, Text, DoubleWritable> {
+
+        /** collect the data for each movie, then do the multiplication
+         *
+         * @param key Movie ID
+         * @param values <movieID_A=relation, movieID_C=relation... userID_A:rating, userID_B:rating...>
+         * @param context key is userID:movieID, value is rating*relation
+         * @throws IOException
+         * @throws InterruptedException
+         */
+        @Override
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+
+            Map<String, Double> relationMap = new HashMap<>(); // movie id and relation
+            Map<String, Double> ratingMap = new HashMap<>();   // user id and rating
+
+            for (Text value : values) {
+                String data = value.toString().trim();
+
+                if (data.contains("=")) {
+                    String[] movieRelation = data.split("=");
+                    try {
+                        Double ralation = Double.parseDouble(movieRelation[1]);
+                        relationMap.put(movieRelation[0], ralation);
+                    } catch (NumberFormatException e) {
+                        continue;
+                    }
+                } else if (data.contains(":")) {
+                    String[] userRating = data.split(":");
+
+                    try {
+                        Double rating = Double.parseDouble(userRating[1]);
+                        ratingMap.put(userRating[0], rating);
+                    } catch (NumberFormatException e) {
+                        continue;
+                    }
+                }
+            }
+
+            for (Map.Entry<String, Double> movieRelation: relationMap.entrySet()) {
+
+                String movieId = movieRelation.getKey();
+                Double relation = movieRelation.getValue();
+
+                for (Map.Entry<String, Double> userRating: ratingMap.entrySet()) {
+                    String userId = userRating.getKey();
+                    Double rating = userRating.getValue();
+
+                    Double multiplication = relation * rating;
+                    String outputKey = userId + ":" + movieId;
+                    context.write(new Text(outputKey), new DoubleWritable(multiplication));
+                }
+
+            }
+
+        }
     }
 }
