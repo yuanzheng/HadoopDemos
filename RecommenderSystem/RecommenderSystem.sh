690de134860080217c5a220c1bec748fb03d81ed
Lucas Song <yuanzheng@cs.byu.edu>
2019-07-06 07:55:45 +0800

    Updated Shell Script for RecommenderSystem project

--- RecommenderSystem/RecommenderSystem.sh
+++ RecommenderSystem/RecommenderSystem.sh
@@ -14,7 +14,9 @@ rm /root/src/RecommenderSystem_Output/part-r*
 hadoop jar /root/src/RecommenderSystem-1.0-jar-with-dependencies.jar movieList/input \
                                                                      DataDivideByUserOutput \
                                                                      coOccurrenceMatrixOutput \
-                                                                     normalizationOutput
+                                                                     normalizationOutput \
+                                                                     MultiplicationOutput \
+                                                                     sumOutput
 
 echo -e "\nTest RecommenderSystem-1.0:"
 
@@ -25,6 +27,10 @@ echo -e "\ncoOccurrenceMatrixOutput:"
 hdfs dfs -ls coOccurrenceMatrixOutput
 echo -e "\nnormalizationOutput:"
 hdfs dfs -ls normalizationOutput
+echo -e "\nMultiplicationOutput:"
+hdfs dfs -ls MultiplicationOutput
+echo -e "\nsumOutput:"
+hdfs dfs -ls sumOutput
 
 # print the output 
 echo -e "\nDataDivideByUser directory:"
@@ -42,7 +48,18 @@ echo -e "\nNormalization directory:"
 hdfs dfs -get normalizationOutput/part-r-00000 /root/src/RecommenderSystem_Output
 mv /root/src/RecommenderSystem_Output/part-r-00000 /root/src/RecommenderSystem_Output/normalizationOutput_3
 
+# print the output 
+echo -e "\nMultiplication directory:"
+hdfs dfs -get MultiplicationOutput/part-r-00000 /root/src/RecommenderSystem_Output
+mv /root/src/RecommenderSystem_Output/part-r-00000 /root/src/RecommenderSystem_Output/MultiplicationOutput_4
+
+# print the output 
+echo -e "\nSum directory:"
+hdfs dfs -get sumOutput/part-r-00000 /root/src/RecommenderSystem_Output
+mv /root/src/RecommenderSystem_Output/part-r-00000 /root/src/RecommenderSystem_Output/sumOutput_5
 
 hdfs dfs -rm -r DataDivideByUserOutput/
 hdfs dfs -rm -r coOccurrenceMatrixOutput/
 hdfs dfs -rm -r normalizationOutput/
+hdfs dfs -rm -r MultiplicationOutput/
+hdfs dfs -rm -r sumOutput/
